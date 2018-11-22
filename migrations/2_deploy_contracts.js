6911a6279f9ecdde9c85a46b4274f57ea61396b0
Daniel Hohner <dah@adorsys.de>
2018-11-22 12:29:14 +0100

    added lending-board to contracts, changed truffle migrations to also deploy lending-board

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 var Base = artifacts.require("./Base.sol");
+var LendingBoard = artifacts.require("./Lending_board.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(Base);
+    deployer.deploy(Base)
+        .then(function () {
+            var minQuorum = 1;
+            var minsForDebate = 0;
+            var majorityMargin = 50;
+            return deployer.deploy(LendingBoard, minQuorum, minsForDebate, majorityMargin, Base.address);
+        });
 };
