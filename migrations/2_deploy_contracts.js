54c19ad952f3e1c75d9fbcef39ff94c374fe135b
Daniel Hohner <dah@adorsys.de>
2019-01-26 23:50:34 +0100

    use async/await in deployment fixed package-lock bug (switch git to https)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,8 +5,12 @@ const minimumQuorum = 1;
 const minutesForDebate = 30;
 const majorityMargin = 50;
 
-module.exports = function(deployer) {
-    deployer.deploy(LendingBoard, minimumQuorum, minutesForDebate, majorityMargin).then(function() {
-        return deployer.deploy(Base, LendingBoard.address);
-    });
+module.exports = async deployer => {
+    await deployer.deploy(
+        LendingBoard,
+        minimumQuorum,
+        minutesForDebate,
+        majorityMargin
+    );
+    await deployer.deploy(Base, LendingBoard.address);
 };
