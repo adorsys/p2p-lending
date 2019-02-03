4b2c26ebb3db7a0564ef823d1ee8717441908f8c
Matthias Freßdorf <mafr@adorsys.de>
2019-02-04 00:03:49 +0100

    Add travis-ci config; Update truffle config for ropsten deployments

--- truffle-config.js
+++ truffle-config.js
@@ -1,32 +1,11 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura API
- * keys are available for free at: infura.io/register
- *
- *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
- *
- *   > > $ npm install truffle-hdwallet-provider@web3-one
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const MNEMONIC = process.env.MNEMONIC;
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const NETWORK_IDS = {
+  // mainnet: 1,
+  ropsten: 3
+};
 
 module.exports = {
   /**
@@ -100,4 +79,11 @@ module.exports = {
       // }
     }
   }
-}
+};
+
+for (let networkName in NETWORK_IDS) {
+    module.exports.networks[ networkName ] = {
+      provider: new HDWalletProvider(MNEMONIC, 'https://' + networkName + '.infura.io/v3/' + INFURA_API_KEY),
+      network_id: NETWORK_IDS[ networkName ]
+    };
+  }
