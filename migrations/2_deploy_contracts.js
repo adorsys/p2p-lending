21ec563da66e449b9c42fab10ad906401f4f2a79
Vladimir Nechai <vne@adorsys.de>
2019-02-05 18:47:21 +0100

    adjust deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,17 @@
-// const Base = artifacts.require("./Base.sol");
-// const LendingBoard = artifacts.require("./LendingBoard.sol");
+const LendingBoard = artifacts.require("./LendingBoard.sol");
+const RequestManagement = artifacts.require("./RequestManagement.sol");
 
-const LendingRequestFactory = artifacts.require("./LendingRequestFactory.sol");
-// const LendingRequest = artifacts.require("./LendingRequest");
-
-// const minimumQuorum = 1;
-// const minutesForDebate = 30;
-// const majorityMargin = 50;
+const minimumQuorum = 1;
+const minutesForDebate = 30;
+const majorityMargin = 50;
 
 module.exports = async deployer => {
-    // await deployer.deploy(
-    //     LendingBoard,
-    //     minimumQuorum,
-    //     minutesForDebate,
-    //     majorityMargin
-    // );
-    // await deployer.deploy(Base, LendingBoard.address);
-    await deployer.deploy(LendingRequestFactory);
+    await deployer.deploy(
+        LendingBoard,
+        minimumQuorum,
+        minutesForDebate,
+        majorityMargin
+    );
+
+    await deployer.deploy(RequestManagement, LendingBoard.address);
 };
