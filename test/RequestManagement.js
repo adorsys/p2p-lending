a79a923b51ae92502ad47c83d22eb6bedafe8a6e
Vladimir Nechai <vne@adorsys.de>
2019-02-06 17:48:30 +0100

    request created but not granted and not settled

--- test/RequestManagement.js
+++ test/RequestManagement.js
@@ -1,5 +1,6 @@
 const LendingBoard = artifacts.require("./LendingBoard.sol");
 const RequestManagement = artifacts.require("./RequestManagement.sol");
+const LendingRequest = artifacts.require("./LendingRequest.sol");
 
 
 contract("RequestManagement", function(accounts) {
@@ -19,10 +20,11 @@ contract("RequestManagement", function(accounts) {
         lendingBoard = await LendingBoard.new(5, 60, 5);
         requestManagement = await RequestManagement.new(lendingBoard.address);
 
+        firstAccount = accounts[0];
         asker = accounts[1];
         lender = accounts[2];
 
-        // await requestManagement.ask(askAmount, paybackAmount, purpose, {from: asker});
+        await requestManagement.ask(askAmount, paybackAmount, purpose, {from: asker});
     });
 
     it("contracts deployed", async () => {
@@ -37,5 +39,37 @@ contract("RequestManagement", function(accounts) {
             0x0,
             "base contract does not have address"
         );
+    });
+
+
+    it("lending request successfully submitted but not granted and not settled", async () => {
+
+        let lendingRequestsNumber = await requestManagement.openLendingRequests.call(asker);
+
+        let lendingRequestAddresses = await requestManagement.getRequests(asker, {from: firstAccount});
+
+        let lendingRequest = await LendingRequest.at(lendingRequestAddresses[lendingRequestsNumber - 1]);
+
+        let amountAsked = await lendingRequest.amountAsked.call();
+
+        console.log(amountAsked);
+
+        // check asker address
+        expect(lendingRequest[0]).to.equal(asker);
+        //
+        // // check if ask amount is the same
+        // expect(lendingRequest[1].toNumber()).to.equal(askAmount);
+        //
+        // // check if payback amount is the same
+        // expect(lendingRequest[2].toNumber()).to.equal(paybackAmount);
+        //
+        // // check that lending request has not been settled yet
+        // expect(lendingRequest[4]).to.equal(false);
+        //
+        // // check lending request purpose
+        // expect(lendingRequest[5]).to.equal(purpose);
+        //
+        // // check that lending request has not been granted yet
+        // expect(lendingRequest[6]).to.equal(false);
     });
 });
