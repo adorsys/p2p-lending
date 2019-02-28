a7655e7232cb962cb39aab399871ff8438dacd94
Daniel Hohner <dah@adorsys.de>
2019-02-28 12:03:07 +0100

    added documentation of functionality

--- contracts/ProposalFactory/ContractFeeProposal.sol
+++ contracts/ProposalFactory/ContractFeeProposal.sol
@@ -4,7 +4,7 @@ contract ContractFeeProposal {
     /// variables
 
     address private author;
-    address private management = address(0);
+    address payable private management = address(0);
     uint256 private proposedFee;
     uint256 private majorityMargin;
     uint256 private minimumNumberOfVotes;
@@ -28,7 +28,7 @@ contract ContractFeeProposal {
         uint256 _proposedFee,
         uint256 _minimumNumberOfVotes,
         uint256 _majorityMargin,
-        address _managementContract
+        address payable _managementContract
     ) public {
         author = _author;
         proposedFee = _proposedFee;
@@ -39,41 +39,60 @@ contract ContractFeeProposal {
 
     /// external
 
+    /**
+     * @notice destroys the proposal contract and forwards the remaining funds to the management contract
+     */
+
     function kill() external {
         require(msg.sender == management, "not called by management contract");
         require(proposalExecuted, "proposal has to be executed first");
-        selfdestruct(msg.sender);
+        selfdestruct(management);
     }
 
-    /// public
-
     /**
-     * @return bool proposalPassed, bool proposalExecuted
+     * @notice registers a vote for the proposal and triggers execution if conditions are met
+     * @param _stance true for a positive vote - false otherwise
+     * @param _origin the address of the initial function call
+     * @return propPassed true if proposal met the required number of positive votes - false otherwise
+     * @return propExecuted true if proposal met the required minimum number of votes - false otherwise
      */
 
-    function vote(bool _stance, address _origin) public returns (bool, bool) {
+    function vote(bool _stance, address _origin) external returns (bool propPassed, bool propExecuted) {
+        // check input parameters
         require(msg.sender == management, "not called by management contract");
         require(!proposalExecuted, "proposal was executed");
         require(!voted[_origin], "you can only vote once");
 
+        // update internal state
         voted[_origin] = true;
         numberOfVotes += 1;
 
         if (_stance == true) {
             numberOfPositiveVotes += 1;
         }
+
+        // check if execution of proposal should be triggered and update return values
         if ((numberOfVotes >= minimumNumberOfVotes)) {
             execute();
             if (proposalPassed) {
-                return(true, true);
+                propPassed = true;
+                propExecuted = true;
             } else {
-                // proposalPassed = false | proposalExecuted = true
-                return(false, true);
+                propPassed = false;
+                propExecuted = false;
             }
         } else {
-            return (false, false);
+            propPassed = false;
+            propExecuted = false;
         }
     }
+
+    /// public
+
+    /**
+     * @notice gets the proposed new contract fee
+     * @return the proposed new fee
+     */
 
     function getContractFee() public view returns (uint256) {
         return proposedFee;
@@ -82,13 +101,19 @@ contract ContractFeeProposal {
     /// internal
     /// private
 
+    /**
+     * @notice executes the proposal and updates the internal state
+     */
+
     function execute() private {
+        // check internal state
         require(!proposalExecuted, "proposal was executed");
         require(
             numberOfVotes >= minimumNumberOfVotes,
             "requirements for execution not met"
         );
 
+        // update the internal state
         proposalExecuted = true;
         
         if (((numberOfPositiveVotes * 100) / numberOfVotes) >= majorityMargin) {
