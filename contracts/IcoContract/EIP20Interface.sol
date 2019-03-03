d00f0f5aa2e97a1633cfba535e59a19832587c06
Ibragim Sautiev <isa@adorsys.de>
2019-03-03 14:45:34 +0100

    erc20 interface eng comments

--- contracts/IcoContract/EIP20Interface.sol
+++ contracts/IcoContract/EIP20Interface.sol
@@ -1,42 +1,42 @@
-// Interface Contract für den ERC20-Token-Standard
+// Abstract contract for the full ERC 20 Token standard
 // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
-pragma solidity ^0.5.0; //Solidity-Compiler Version
+pragma solidity ^0.5.0;
 
 
 contract EIP20Interface {
-
-    /// gesamte Menge der Token
+  
+    /// total amount of tokens
     uint256 public totalSupply;
 
-    /// @param _owner Die Adresse von welcher das Tokenguthaben abgefragt wird
-    /// @return Das Tokenguthaben
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
     function balanceOf(address _owner) public view returns (uint256 balance);
 
-    /// @notice Verschicke die Menge '_value' an Token an '_to' vom 'msg.sender'
-    /// @param _to Die Adresse des Empfängers
-    /// @param _value Die Menge an Token, welche versendet werden
-    /// @return Rückgabewert bezeugt den Erfolg der Transaktion
+    /// @notice send '_value' token to '_to' from 'msg.sender'
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) public returns (bool success);
 
-    /// @notice Verschicke die Menge '_value' an Token an to '_to' vom '_from' unter der Bedingung, '_from' akzeptiert dies
-    /// @param _from Die Adresse des Absenders
-    /// @param _to Die Adresse des Empfängers
-    /// @param _value Die Menge an Token, welche versendet werden
-    /// @return Rückgabewert bezeugt den Erfolg der Transaktion
+    /// @notice send '_value' token to '_to' from '_from' on the condition it is approved by '_from'
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
 
-    /// @notice 'msg.sender' akzeptiert, dass '_spender' die Menge '_value' an Token versedet
-    /// @param _spender Die Adresse des Kontos, welches die Transaktion tätigen darf
-    /// @param _value Die Menge an Token, welche versendet werden dürfen
-    /// @return Rückgabewert bezeugt den das Einverständnis von 'msg.sender' 
+    /// @notice 'msg.sender' approves '_spender' to spend '_value' tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) public returns (bool success);
 
-    /// @param _owner Die Adresse des Kontos, welches die Token besitzt
-    /// @param _spender Die Adresse des Kontos, welches die Transaktion tätigen darf
-    /// @return Die Menge an restlichen Token, welche transferiert werden dürfen
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
-    // Events protokollieren die Transaktionen und Einverständniserklärungen 
+    // Display transactions and approvals
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
