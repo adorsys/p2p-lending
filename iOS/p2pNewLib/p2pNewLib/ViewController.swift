93bf4fd236dad58327c065e749ff7892dc47a16f
Stefan Haßferter <sth@adorsys.de>
2019-03-20 12:50:01 +0100

    add vote function

--- iOS/p2pNewLib/p2pNewLib/ViewController.swift
+++ iOS/p2pNewLib/p2pNewLib/ViewController.swift
@@ -12,6 +12,9 @@ import Web3
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,20 +25,70 @@ class ViewController: UIViewController {
         print(Web3Response)
         }
 
+        let privateKey = try! EthereumPrivateKey(hexPrivateKey: "0xb7553adc227436ddd9a41fa114f2248cc6247f80276726c3450b93861866d8b0")
+        guard let jsonAbi = loadAbi() else {return}
+
         let contractAddress = EthereumAddress(hexString: "0x24aA30159759eE23421AA808c5d50998736D346e")
+        let contract = try! etherNode.eth.Contract(json: jsonAbi, abiKey: nil, address: contractAddress)
 
 
-        let contract = try! etherNode.eth.Contract(json: jsonABI.data(using: .utf8)!, abiKey: nil, address: contractAddress)
-
-        let test = contract["loadProposalCount"]!().call { (response, error) in
-            let test2 = 0
-            if let response = response,
-                let count = response["_count"]{
-                print(count)
-            }
+        let loadPropsalCount = contract["loadProposalCount"]!().createCall()
+        contract.call(loadPropsalCount!, outputs: [.init(name: "_count", type: .uint8)]) { (response, error) in
+            print(response!)
         }
 
-        print(test)
+        let winningProposal = contract["winningProposal"]!().createCall()
+        contract.call(winningProposal!, outputs: [.init(name: "_winningProposal", type: .uint8),
+                                                  .init(name: "_winningVoteCount", type: .uint8)]) { (response, error) in
+            print(response!)
+        }
+
+
+        let c = contract["vote"]?(BigUInt(2))
+
+
+
+        etherNode.eth.getTransactionCount(address: privateKey.address, block: .latest){ response in
+            print (response.result!.quantity)
+
+            let trans = c!.createTransaction(nonce: response.result!,
+                                             from: privateKey.address,
+                                             value: 0,
+                                             gas: 38182,
+                                             gasPrice: EthereumQuantity(quantity: 1.gwei))
+
+
+            let signedTx = try! trans?.sign(with: privateKey,chainId: 5777)
+
+            etherNode.eth.sendRawTransaction(transaction: signedTx!) { response in
+                print(response)
+            }
+
+
+            contract.call(winningProposal!, outputs: [.init(name: "_winningProposal", type: .uint8),
+                                                      .init(name: "_winningVoteCount", type: .uint8)]) { (response, error) in
+                                                        print(response!)
+            }
+
+        }
+
+
+
+
+//        contract.call(c, outputs: [.init(name: "toProposal", type: .uint8)]) { (response, error) in
+//                                                    print(response!)
+//        }
     }
 }
 
+extension ViewController{
+
+    func loadAbi()->Data?{
+        if let path = Bundle.main.path(forResource: "Abi", ofType: "json"),
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path)) {
+            return data
+        }
+        return nil
+    }
+
+}
